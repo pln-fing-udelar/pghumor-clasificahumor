fc89e28d2ac238b8a11ab04f9b981903dca4d672
bryant1410 <bryant@montevideo.com.uy>
2014-08-20 13:49:17 -0300

    cambio menor

--- index.php
+++ index.php
@@ -71,7 +71,7 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="dist/js/bootstrap.min.js"></script>
     <script>
     if (document.location.hostname.search("clasificachistes.com") !== -1) { // Enable GA only in production.
 
