f6036a5575224f5a5e9e13dc4a2acdc01bf140a7
Santiago Castro <santi.1410@hotmail.com>
2018-02-20 00:29:30 -0300

    Add missing charset, autocommit and cursor closing

--- clasificahumor/database.py
+++ clasificahumor/database.py
@@ -5,7 +5,7 @@ from typing import Any, Dict, List
 import MySQLdb
 
 db = MySQLdb.connect(host=os.getenv('DB_HOST'), user=os.getenv('DB_USER'), password=os.getenv('MYSQL_ROOT_PASSWORD'),
-                     database=os.getenv('DB_NAME'))
+                     database=os.getenv('DB_NAME'), charset='utf8mb4', autocommit=True)
 
 
 # TODO: test with not voted tweets to see what happens. Create tests?
@@ -19,16 +19,16 @@ def random_least_voted_unseen_tweets(session_id: str, batch_size: int) -> List[D
     :param batch_size: Size of the list to return
     :return: Random list of the least voted unseen tweets with size batch_size
     """
-    cursor = db.cursor()
-    cursor.execute('SELECT t.tweet_id, text'
-                   ' FROM tweets t'
-                   '   LEFT JOIN (SELECT tweet_id, session_id FROM votes WHERE session_id != %(session_id)s) a'
-                   '     ON t.tweet_id = a.tweet_id'
-                   ' GROUP BY t.tweet_id'
-                   ' ORDER BY COUNT(a.session_id), RAND()'
-                   ' LIMIT %(limit)s',
-                   {'session_id': session_id, 'limit': batch_size})
-    return [{'id': id_, 'text': text} for id_, text in cursor.fetchall()]
+    with db.cursor() as cursor:
+        cursor.execute('SELECT t.tweet_id, text'
+                       ' FROM tweets t'
+                       '   LEFT JOIN (SELECT tweet_id, session_id FROM votes WHERE session_id != %(session_id)s) a'
+                       '     ON t.tweet_id = a.tweet_id'
+                       ' GROUP BY t.tweet_id'
+                       ' ORDER BY COUNT(a.session_id), RAND()'
+                       ' LIMIT %(limit)s',
+                       {'session_id': session_id, 'limit': batch_size})
+        return [{'id': id_, 'text': text} for id_, text in cursor.fetchall()]
 
 
 def random_tweets(batch_size: int) -> List[Dict[str, Any]]:
@@ -40,13 +40,13 @@ def random_tweets(batch_size: int) -> List[Dict[str, Any]]:
     :param batch_size: Size of the list to return
     :return: Random list of tweets with size batch_size
     """
-    cursor = db.cursor()
-    cursor.execute('SELECT t.tweet_id, text'
-                   ' FROM tweets t'
-                   ' ORDER BY RAND()'
-                   ' LIMIT %(limit)s',
-                   {'limit': batch_size})
-    return [{'id': id_, 'text': text} for id_, text in cursor.fetchall()]
+    with db.cursor() as cursor:
+        cursor.execute('SELECT t.tweet_id, text'
+                       ' FROM tweets t'
+                       ' ORDER BY RAND()'
+                       ' LIMIT %(limit)s',
+                       {'limit': batch_size})
+        return [{'id': id_, 'text': text} for id_, text in cursor.fetchall()]
 
 
 def add_vote(session_id: str, tweet_id: str, vote: str) -> None:
@@ -60,7 +60,7 @@ def add_vote(session_id: str, tweet_id: str, vote: str) -> None:
     :param vote: Vote of the tweet: '1' to '5' for the stars, 'x' for non-humorous and 'n' for skipped
     """
     if vote in ['1', '2', '3', '4', '5', 'x', 'n']:
-        cursor = db.cursor()
-        cursor.execute('INSERT INTO votes (tweet_id, session_id, vote)'
-                       ' VALUES (%(tweet_id)s, %(session_id)s, %(vote)s)',
-                       {'tweet_id': tweet_id, 'session_id': session_id, 'vote': vote})
+        with db.cursor() as cursor:
+            cursor.execute('INSERT INTO votes (tweet_id, session_id, vote)'
+                           ' VALUES (%(tweet_id)s, %(session_id)s, %(vote)s)',
+                           {'tweet_id': tweet_id, 'session_id': session_id, 'vote': vote})
